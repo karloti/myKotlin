73f27ca9202748daf53647e9f777a0b664b3d672
Karloti <skarloti@gmail.com>
2021-04-11 18:07:09 +0300

    modified

--- src/main/kotlin/quickSort.kt
+++ src/main/kotlin/quickSort.kt
@@ -12,27 +12,29 @@ class QuickSort(private val a: IntArray) {
 
     private fun partition(lowIndex: Int, highIndex: Int): Pair<Int, Int> {
         val middleIndex = (lowIndex + highIndex) / 2
-        var pivotIndex = when {
+        val pivotIndex = when {
             a[lowIndex] <= a[highIndex] == a[highIndex] <= a[middleIndex] -> highIndex
             a[lowIndex] <= a[middleIndex] == a[middleIndex] <= a[highIndex] -> middleIndex
             else -> lowIndex
         }
         val pivot = a[pivotIndex]
-        var pivotCount = 1
+        var pivotLowIndex = lowIndex
+        var pivotHighIndex = highIndex
 
-        swap(pivotIndex, highIndex)
-        pivotIndex = lowIndex
+        for (i in lowIndex until pivotIndex)
+            if (a[i] < pivot) swap(i, pivotLowIndex++)
+        swap(pivotLowIndex, pivotIndex)
 
-        for (i in lowIndex until highIndex) when {
-            a[i] < pivot -> swap(i, pivotIndex++)
-            a[i] == pivot -> pivotCount++
-        }
-        swap(highIndex, pivotIndex)
+        for (i in pivotHighIndex downTo pivotLowIndex + 1)
+            if (a[i] > pivot) swap(i, pivotHighIndex--)
 
-        var i = pivotIndex
-        var count = pivotCount - 1
-        while (count > 0 && i++ <= highIndex) if (a[i] == pivot) swap(i, pivotIndex + count--)
-        return pivotIndex to pivotIndex + pivotCount - 1
+        swap(pivotLowIndex, pivotHighIndex)
+        val index = pivotLowIndex
+        pivotLowIndex = pivotHighIndex
+        for (i in pivotHighIndex - 1 downTo index)
+            if (a[i] == pivot) swap(i, --pivotLowIndex)
+
+        return pivotLowIndex to pivotHighIndex
     }
 
     private fun sort(lowIndex: Int, highIndex: Int) {
@@ -45,8 +47,8 @@ class QuickSort(private val a: IntArray) {
 }
 
 fun main() {
-    val size = 300_000_000
-    val a1 = IntArray(size) { (1..size / 10).random() }
+    val size = 30_000_000
+    val a1 = IntArray(size) { (1..size).random() / (1..10).random() }
     val a2 = a1.clone()
 
     println(a1.joinToString(" ", "a1 = [", "]", 20, " ... of ${a1.size - 20}") { "%d".format(it) })
@@ -66,4 +68,6 @@ fun main() {
         println(a2.joinToString(" ", "a2 = [", "]", 20, " ... of ${a2.size - 20}") { "%d".format(it) })
         println("Finished $it ms")
     }
+
+    println(a1.contentEquals(a2))
 }
