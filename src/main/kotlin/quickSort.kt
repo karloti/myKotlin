72cbb301d077918a202c177c0981d58edf2d65c8
Karloti <skarloti@gmail.com>
2021-04-11 20:38:55 +0300

    Fastest

--- src/main/kotlin/quickSort.kt
+++ src/main/kotlin/quickSort.kt
@@ -10,45 +10,34 @@ class QuickSort(private val a: IntArray) {
         sort(0, a.lastIndex)
     }
 
-    private fun partition(lowIndex: Int, highIndex: Int): Pair<Int, Int> {
+    private fun partition(lowIndex: Int, highIndex: Int): Int {
         val middleIndex = (lowIndex + highIndex) / 2
-        val pivotIndex = when {
+        var pivotIndex = when {
             a[lowIndex] <= a[highIndex] == a[highIndex] <= a[middleIndex] -> highIndex
             a[lowIndex] <= a[middleIndex] == a[middleIndex] <= a[highIndex] -> middleIndex
             else -> lowIndex
         }
         val pivot = a[pivotIndex]
-        var pivotLowIndex = lowIndex
-        var pivotHighIndex = highIndex
-
-        for (i in lowIndex until pivotIndex)
-            if (a[i] < pivot) swap(i, pivotLowIndex++)
-        swap(pivotLowIndex, pivotIndex)
-
-        for (i in pivotHighIndex downTo pivotLowIndex + 1)
-            if (a[i] > pivot) swap(i, pivotHighIndex--)
-
-        swap(pivotLowIndex, pivotHighIndex)
-        val index = pivotLowIndex
-        pivotLowIndex = pivotHighIndex
-        for (i in pivotHighIndex - 1 downTo index)
-            if (a[i] == pivot) swap(i, --pivotLowIndex)
-
-        return pivotLowIndex to pivotHighIndex
+        swap(pivotIndex, highIndex)
+        pivotIndex = lowIndex
+        for (i in lowIndex until highIndex)
+            if (a[i] < pivot) swap(i, pivotIndex++)
+        swap(highIndex, pivotIndex)
+        return pivotIndex
     }
 
     private fun sort(lowIndex: Int, highIndex: Int) {
         if (lowIndex < highIndex) {
-            val (pivotLowIndex, pivotHighIndex) = partition(lowIndex, highIndex)
-            sort(lowIndex, pivotLowIndex - 1)
-            sort(pivotHighIndex + 1, highIndex)
+            val pivotIndex = partition(lowIndex, highIndex)
+            sort(lowIndex, pivotIndex - 1)
+            sort(pivotIndex + 1, highIndex)
         }
     }
 }
 
 fun main() {
-    val size = 30_000_000
-    val a1 = IntArray(size) { (1..size).random() / (1..10).random() }
+    val size = 100_000_000
+    val a1 = IntArray(size) { (1..size).random() }
     val a2 = a1.clone()
 
     println(a1.joinToString(" ", "a1 = [", "]", 20, " ... of ${a1.size - 20}") { "%d".format(it) })
@@ -61,7 +50,7 @@ fun main() {
         println("Finished $it ms")
     }
 
-    println("\nStart Karlo Second!")
+    println("\nStart Karlo!")
     measureTimeMillis {
         QuickSort(a2)
     }.let {
@@ -69,5 +58,5 @@ fun main() {
         println("Finished $it ms")
     }
 
-    println(a1.contentEquals(a2))
+    println("\nEquality: " + a1.contentEquals(a2))
 }
